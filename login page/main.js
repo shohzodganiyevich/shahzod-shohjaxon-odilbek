da18ef12710abe88ad6de38d4ca52f520c9bf758
Shohjahon <shoxjahonzohirov2010@gmail.com>
2025-07-10 17:51:40 +0500

    Login page faqat css, back endi qilinadi

---
+++ login page/main.js
@@ -0,0 +1,7 @@
+const api = axios.create({
+  baseURL: "https://686e83e791e85fac429e127a.mockapi.io",
+});
+
+let name = document.querySelector(".name");
+let email = document.querySelector(".email");
+let password = document.querySelector(".password");
